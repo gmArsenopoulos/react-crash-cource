7fbcbda12f613097dc0f37617c9fa9082e5819fb
gmarsenopoulos <gmarsenopoulos@gmail.com>
2025-11-07 17:53:02 +0200

    dynamic title for jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -7,7 +7,7 @@ const JobListings = ({widget=false}) => {
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {widget ? 'Recent Jobs':'Browse Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {jobs.map(job=>(
